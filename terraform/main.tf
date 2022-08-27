04c45b8533e111fd4b5a40e86d315f5a2b481127
Joe Loesch <joe.wr.loesch@gmail.com>
2022-08-27 17:17:11 -0400

    Update, refocus for seperate cluster config repo

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,12 +1,12 @@
 resource "proxmox_vm_qemu" "moose-k3s-master" {
-  count = 1
+  count = 3
   name        = "moose-k3s-master-0${count.index + 1}"
-  target_node = "bender"
-  clone = "ubuntu-cloud-bender"
+  target_node = "baymax"
+  clone = "ubuntu-cloud-shared-baymax"
 
   sockets = 1
-  cores = 3
-  memory = 4096
+  cores = 2
+  memory = 2048
 
   sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEn5k5xqBqVg9HqNwOq/TjtvIUc+/vugkDh6PVeI7FYg joe@Joes-MacBook-Pro.local"
 
@@ -15,20 +15,20 @@ resource "proxmox_vm_qemu" "moose-k3s-master" {
 
   disk {
     type         = "virtio"
-    storage      = "fast-thin"
+    storage      = "baymax-fast"
     size         = "100G"
   }
 }
 
 resource "proxmox_vm_qemu" "moose-k3s-worker" {
-  count = 3
+  count = 2
   name        = "moose-k3s-worker-0${count.index + 1}"
-  target_node = "bender"
-  clone = "ubuntu-cloud-bender"
+  target_node = "baymax"
+  clone = "ubuntu-cloud-shared-baymax"
 
-  sockets = 5
-  cores = 1
-  memory = 10240
+  sockets = 3
+  cores = 3
+  memory = 9216
 
   sshkeys = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEn5k5xqBqVg9HqNwOq/TjtvIUc+/vugkDh6PVeI7FYg joe@Joes-MacBook-Pro.local"
 
@@ -37,14 +37,9 @@ resource "proxmox_vm_qemu" "moose-k3s-worker" {
 
   disk {
     type         = "virtio"
-    storage      = "fast-thin"
+    storage      = "baymax-fast"
     size         = "200G"
   }
-#   disk {
-#     type         = "virtio"
-#     storage      = "baymax-fast"
-#     size         = "200G"
-#   }
 }
 
 locals {
